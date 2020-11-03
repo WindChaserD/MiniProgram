8fef6eca716936637889d89d0f0d96f4b42c2b16
WindChaserD <781201469@qq.com>
2020-11-03 16:20:11 +0800

    01-1-小程序基础了解

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      name:'DD',
+      list:[
+        {name:'A',num:1},
+        {name:'B',num:2},
+        {name:'C',num:3}
+      ],
+      counter:0
   },
+  AddNum(){
+      // 错误的更改参数方法
+      // this.data.counter += 1;
+      // console.log(this.data.counter);
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+      //正确的修改并且更新页面的方法
+      this.setData({
+        counter: this.data.counter + 1
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  SubNum(){
+    this.setData({
+      counter: this.data.counter - 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
